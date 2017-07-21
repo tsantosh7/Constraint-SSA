2613cb7adcfd574bad13d3009d4d4eaef3c4efd8
Sailendra <s.ramanna@ucl.ac.uk>
2017-07-21 20:01:36 +0000

    Update get_SSA_Freqs.m Added SSA_WINDOW

--- get_SSA_Freqs.m
+++ get_SSA_Freqs.m
@@ -1,8 +1,11 @@
-function [EEG_Frequencies] = get_SSA_Freqs(X)
+function [EEG_Frequencies] = get_SSA_Freqs(X,SSA_WINDOW)
 
+[EEG_Frequencies.Delta,~] = get_SSA_EEG(X,0.2,3.9,SSA_WINDOW);
+[EEG_Frequencies.Delta,~] = get_SSA_EEG(X,4,7.9, SSA_WINDOW);
+[EEG_Frequencies.Alpha,~] = get_SSA_EEG(X,8,12.9,SSA_WINDOW);
+[EEG_Frequencies.Beta,~] = get_SSA_EEG(X,13,40,SSA_WINDOW);
+[EEG_Frequencies.Gamma,~] = get_SSA_EEG(X,40,60,SSA_WINDOW);
+EEG_Frequencies.Concat = [EEG_Frequencies.Delta;EEG_Frequencies.Delta...
+    EEG_Frequencies.Alpha,EEG_Frequencies.Beta,EEG_Frequencies.Gamma]';
 
-[EEG_Frequencies.Delta] = get_SSA_EEG(X,0.2,3.9);
-[EEG_Frequencies.Theta] = get_SSA_EEG(X,4,7.9);
-[EEG_Frequencies.Alpha] = get_SSA_EEG(X,8,12.9);
-[EEG_Frequencies.Beta] = get_SSA_EEG(X,13,40);
-[EEG_Frequencies.Gamma] = get_SSA_EEG(X,40,60);
+end
